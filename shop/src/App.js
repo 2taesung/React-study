253bdbb4c653ef998d402d21c7374b048ca7f21a
이태성 <dlxotjde@gmail.com>
2021-11-09 20:21:27 +0900

    Add export import

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button, img } from 'react-bootstrap';
 import './App.css';
+import data from './data.js';
 
 function App() {
+
+  let [shoes, shoes변경] = useState(data);
+
   return (
     <div className="App">
+      
       <Navbar bg="light" expand="lg">
       <Container fluid>
         <Navbar.Brand href="#">Shoe Shop</Navbar.Brand>
@@ -49,7 +54,7 @@ function App() {
       <div className="row">
         <div className="col-md-4">
         <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="" width="100%"/>
-          <h4>상품명</h4>
+          <h4>{ shoes[0].title }</h4>
           <p>상품설명 & 가격</p>
         </div>
         <div className="col-md-4">
